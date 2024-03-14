8c271c17ba6f820418a395058ceaa97a64c339a2
cheng chen <cheng.chen1@huawei.com>
2024-03-14 10:33:23 -0700

    Modification on actor

--- qservice/qactor/python/test4.py
+++ qservice/qactor/python/test4.py
@@ -6,7 +6,7 @@ import worker
 import torch
 import torch.nn  as nn
 import pickle
-from mingpt.model import GPT, GPT_P1, GPT_P2
+from gptmodel import GPT, GPT_P1, GPT_P2
 from transformers import GPT2Tokenizer
 from torch.nn import functional as F
 from queue import Queue, Empty 
@@ -34,7 +34,7 @@ class Preprocessor:
         print("Preprocessor: input phrase:",tokenizer.decode(indexed_tokens), "sending to worker1...")
         tokens_tensor = torch.tensor([indexed_tokens]) 
         print("tokens_tensor:", tokens_tensor)
-        worker.ActorSystem.send(worker, "Model_GPT", "generate", reqid, tokens_tensor)
+        worker.ActorSystem.send(worker, "Scheduler", "recv", reqid, tokens_tensor)
 
 class Postprocessor:
     def __init__(self, model):
@@ -49,41 +49,35 @@ class Postprocessor:
         worker.ActorSystem.http_return(worker, "httpactor", "send", reqid, ascii_str)
         #worker.ActorSystem.send(worker, "worker1", "run_model", reqid, tokens_tensor)
 
-queue = Queue()
-
-class Model_GPT:
+class Scheduler:
     def __init__(self, model, max_new_tokens=20, temperature=1.0, do_sample=False, top_k=None):
         self.model = model
         self.max_new_tokens = max_new_tokens
         self.temperature = temperature
         self.do_sample = do_sample
         self.top_k = top_k
-        # self.thread = Thread(target=self.fetch_queue_item)
-        # self.queue = Queue()
-        print("Model_GPT: init ...")
-    
-    # def recv(self, reqid, data):
-    #     print("recv items, try to put into queue")
-    #     deserializaed_tensor = pickle.loads(bytes(data))
-    #     self.queue.put(deserializaed_tensor)
-    #     print("recv items, finish put into queue")
+        self.queue = Queue() # item: tuple or (reqid, tensor)
+        self.thread = Thread(target=self.generate)
+        self.thread.start()
 
-    # def fetch_queue_item(self):
-    #     print("!!!!fetch_queue_item")
-    #     while self.queue.empty():
-    #         continue
-    #     print("!!! finish")
-    #     return
+    def recv(self, reqid, data):
+        deserializaed_tensor = pickle.loads(bytes(data))
+        self.queue.put((reqid, deserializaed_tensor))
 
-    def generate(self, reqid, data):# make it a thread
-        idx = pickle.loads(bytes(data))
+    def try_fetch_queue_item(self):
+        return self.queue.get(True)
+
+    def generate(self):# make it a thread
+        (reqid, idx) = self.try_fetch_queue_item()
         for i in range(int(self.max_new_tokens)):
+            start_time = time.time()
             # if the sequence context is growing too long we must crop it at block_size
             idx_cond = idx if idx.size(1) <= self.model.block_size else idx[:, -self.model.block_size:]
             # forward the model to get the logits for the index in the sequence
             print("Model_GPT: send to worker1")
             worker.ActorSystem.send(worker, "worker1", "run_model", reqid, idx_cond)
-            logits = queue.get(True)
+            
+            (reqid, logits) = self.try_fetch_queue_item()
             # logits, _ = self(idx_cond)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / self.temperature
@@ -101,17 +95,9 @@ class Model_GPT:
             # append sampled index to the running sequence and continue
             idx = idx.to("cuda:1")
             idx = torch.cat((idx, idx_next), dim=1)
+            print("i is:", i, "time elapse is:", time.time()-start_time)
 
         worker.ActorSystem.send(worker, "Postprocessor", "execute", reqid, idx, auto_data_movement=False)
-
-class Model_GPT_HELPER:
-    def __init__(self, model):
-        self.model = model
-        print("Model_GPT_HELPER: init ...")
-
-    def recv_tensor(self, reqid, data):
-        deserializaed_tensor = pickle.loads(bytes(data))
-        queue.put(deserializaed_tensor)
 
 class Worker1:
     def __init__(self, model):
@@ -134,8 +120,8 @@ class Worker2:
         deserializaed_tensor = pickle.loads(bytes(data))
         print("worker2: run_model ...[req:", reqid,"]")
         output = self.model(deserializaed_tensor)
-        worker.ActorSystem.send(worker, "Model_GPT_HELPER", "recv_tensor", reqid, output, auto_data_movement=False)
-        print("worker2: finish ", reqid, ", sending to GPT model")
+        worker.ActorSystem.send(worker, "Scheduler", "recv", reqid, output, auto_data_movement=False)
+        print("worker2: finish ", reqid, ", sending to Scheduler")
         # print(logits.shape)
         # tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         # decoded_output = tokenizer.batch_decode(logits.cpu().detach().numpy().argmax(-1).tolist())
@@ -159,10 +145,10 @@ def main():
 
     system.new_actor("Preprocessor", Preprocessor, dummy_model, use_gpu=False)
     system.new_actor("Postprocessor", Postprocessor, dummy_model, use_gpu=False)
-    system.new_actor("Model_GPT", Model_GPT, model, use_gpu=False)
+    system.new_actor("Scheduler", Scheduler, model, use_gpu=False)
     system.new_actor("worker1", Worker1, model_p1)
     system.new_actor("worker2", Worker2, model_p2)
-    system.new_actor("Model_GPT_HELPER", Model_GPT_HELPER, dummy_model, use_gpu=False)
+    # system.new_actor("Model_GPT_HELPER", Model_GPT_HELPER, dummy_model, use_gpu=False)
     system.new_http_actor("httpactor", "Preprocessor", "execute", 9876)
     system.wait()
 
